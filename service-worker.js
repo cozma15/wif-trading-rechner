94cbf74bbea6e536a164ee2ea701ddcaad4afdb1
cozma15 <153546069+cozma15@users.noreply.github.com>
2025-04-30 19:30:15 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -2,18 +2,16 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open('wif-cache').then((cache) => {
       return cache.addAll([
-        'wif-rechner.html',
+        'index.html',
         'manifest.json',
-        'icon-dollar-192.png',
-        'splash-icon-512.png'
+        'icon-192.png',
+        'splash-512.png'
       ]);
     })
   );
 });
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then((res) => res || fetch(event.request))
   );
 });
